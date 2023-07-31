f585d63975cd29881829740c48bebab099118b61
waniakai <119068349+waniakai@users.noreply.github.com>
2023-07-31 12:23:39 +0530

    Update ChartMian.tsx

--- src/Component/ChartMap/ChartMian.tsx
+++ src/Component/ChartMap/ChartMian.tsx
@@ -5,13 +5,13 @@ import LineGraphCountryCode from './LineGraphCountry';
 
 const ChartMain: React.FC = () => {
   return (
-    <div className='mt-8 ml-4 md:ml-40 flex flex-col z-10'> {/* Added margin, responsive styles, and z-index */}
-      <h1 className='text-2xl md:ml-20 font-bold text-4xl'>Chart And Map</h1> {/* Added font size and bold styles */}
+    <div className='mt-8 ml-4 md:ml-40 flex flex-col z-10'> 
+      <h1 className='text-2xl md:ml-20 font-bold text-4xl'>Chart And Map</h1> 
       <div className='mt-4 md:ml-20'>
         <LineGraph />
         <LineGraphCountryCode />
       </div>
-      <div className='mt-4 mr-2 md:ml-20 mb-8 relative z-0'> {/* Added bottom margin, relative positioning, and z-index */}
+      <div className='mt-4 mr-2 md:ml-20 mb-8 relative z-0'>
         <Map />
       </div>
       <div className='h-20'>
